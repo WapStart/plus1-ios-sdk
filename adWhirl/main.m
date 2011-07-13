a0c96dbf350e7b8f05d032bf57c6c380fb338c64
Alexander A. Klestov <alan@klestoff.ru>
2011-07-13 15:30:10 +0000

    adWhirl application example

---
+++ adWhirl/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  adWhirl
+//
+//  Created by cookie on 17/11/2010.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
