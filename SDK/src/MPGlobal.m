645ba6762b2ad9d51218ab290eeb879b30990c22
Alexander A. Zaytsev <a.zaytsev@co.wapstart.ru>
2012-04-20 13:04:21 +0000

    remove unused methods

--- SDK/src/MPGlobal.m
+++ SDK/src/MPGlobal.m
@@ -53,41 +53,6 @@ CGRect MPScreenBounds()
 	return bounds;
 }
 
-CGFloat MPDeviceScaleFactor()
-{
-	if ([[UIScreen mainScreen] respondsToSelector:@selector(displayLinkWithTarget:selector:)] &&
-		[[UIScreen mainScreen] respondsToSelector:@selector(scale)])
-	{
-		return [[UIScreen mainScreen] scale];
-	}
-	else return 1.0;
-}
-
-NSString *MPHashedUDID()
-{
-	static NSString *hashedUDID = nil;
-	
-	if (!hashedUDID) 
-	{
-		unsigned char digest[20];
-		
-		NSString *udid = [NSString stringWithFormat:@"%@", 
-						  [[UIDevice currentDevice] uniqueIdentifier]];
-		NSData *data = [udid dataUsingEncoding:NSASCIIStringEncoding];
-		CC_SHA1([data bytes], [data length], digest);
-		
-		NSMutableString *output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
-		
-		for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) 
-		{
-			[output appendFormat:@"%02x", digest[i]];
-		}
-		
-		hashedUDID = [[NSString stringWithFormat:@"sha:%@", [output uppercaseString]] retain];
-	}
-	return hashedUDID;
-}
-
 NSString *MPUserAgentString()
 {
 	static NSString *userAgent = nil;
@@ -113,16 +78,3 @@ NSDictionary *MPDictionaryFromQueryString(NSString *query) {
 	return queryDict;
 }
 
-@implementation NSString (MPAdditions)
-
-- (NSString *)URLEncodedString
-{
-	NSString *result = (NSString *)CFURLCreateStringByAddingPercentEscapes(NULL,
-																		   (CFStringRef)self,
-																		   NULL,
-																		   (CFStringRef)@"!*'();:@&=+$,/?%#[]<>",
-																		   kCFStringEncodingUTF8);
-	return [result autorelease];
-}
-
-@end
