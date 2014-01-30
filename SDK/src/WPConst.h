ac5dac56c8660973826b31f140f2586e3f8b741e
Alexander A. Klestov <alan@klestoff.ru>
2014-01-30 18:05:55 +0400

    prepare to 2.1.3 release

--- SDK/src/WPConst.h
+++ SDK/src/WPConst.h
@@ -44,7 +44,7 @@
 
 #define HTML_NO_BANNER @"<!-- i4jgij4pfd4ssd -->"
 
-#define WPRotatorUrl @"http://ro.plus1.wapstart.ru/?area=applicationWebView&version=2&sdkver=2.1.2"
+#define WPRotatorUrl @"http://ro.plus1.wapstart.ru/?area=applicationWebView&version=2&sdkver=2.1.3"
 #define WPSessionKey @"WPClientSessionId"
 
 #endif
